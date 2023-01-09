22a2d9fcc11475216db0e9a65317f0ac9b45c721
Behnoosh Moshtagh <behnoosh.moshtagh@gmail.com>
2023-01-09 12:16:52 +0330

    Change NavBar

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,6 @@ Route::get('/', function () {
 });
 
 Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    'auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+        Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
 });
